b505c6a373409487ea38340e001d7f6619f0147b
iris <1312586226@qq.com>
2026-05-11 22:03:19 +0800

    fix: update base path for netlify

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/life-imprint-fronted/',
+  base: '/', // 改成根路径，Netlify 部署必须这样写
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
